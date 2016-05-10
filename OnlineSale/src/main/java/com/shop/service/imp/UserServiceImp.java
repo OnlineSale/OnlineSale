e5a8478073e520ad86041cb9244ca0cf491b51b4
xiechur <18826077190@163.com>
2016-05-10 22:46:39 +0800

    用户信息userInfo 和 用户saveInfo

--- OnlineSale/src/main/java/com/shop/service/imp/UserServiceImp.java
+++ OnlineSale/src/main/java/com/shop/service/imp/UserServiceImp.java
@@ -1,12 +1,11 @@
 package com.shop.service.imp;
 
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
 import com.shop.dao.UserMapper;
 import com.shop.model.User;
 import com.shop.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class UserServiceImp implements UserService{
@@ -19,4 +18,23 @@ public class UserServiceImp implements UserService{
 		userDao.insert(user);
 	}
 
+	@Override
+	public int update(User user) {
+		return userDao.updateByPrimaryKeySelective(user);
+	}
+
+	@Override
+	public User find(int id) {
+		return userDao.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public User findUserInfo(int id) {
+		return userDao.findUserInfoById(id);
+	}
+
+	@Override
+	public User login(User user) {
+		return userDao.login(user);
+	}
 }
